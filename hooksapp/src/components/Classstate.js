0263fcf93a3d82fb1b5f433438c162d90df88421
[Matias] <monteil.mathias@gmail.com>
2024-04-16 11:04:07 +0200

    correction class

--- hooksapp/src/components/Classstate.js
+++ hooksapp/src/components/Classstate.js
@@ -6,24 +6,23 @@ import React, { Component, useState } from 'react';
 // this.handleClick = this.handleClick.bind(this);
 // }
 
-const addOne = () => {
-    this.setState.counter ({
-        counter: this.state.counter + 1
-    })
-}
 // const addOne = () => {
-//     this.setState(prevState => {
-//         return{
-//         counter: prevState.counter + 1
-//     }
+//     this.setState.counter ({
+//         counter: this.state.counter + 1
 //     })
 // }
+
 
 class ClassState extends Component{
     state={
         counter : 0
     }
-
+    addOne = () => {
+        this.setState(prevState => {
+            return{
+            counter: prevState.counter + 1
+        }})
+    }
     render() {
         return (
             <div>
