552ea1403f63b1904e50d86db0cf98b9a51b6483
[Matias] <monteil.mathias@gmail.com>
2024-04-19 23:32:06 +0200

    s

--- hooksapp/src/components/FunctionCount.js
+++ hooksapp/src/components/FunctionCount.js
@@ -2,27 +2,25 @@ import React, {useState, useEffect} from 'react'
 
 function FunctionCount() {
 
-    const [count, setCount] = useState(0)
-    const[name, setName] = useState("")
+  const [count, setCount] = useState(0)
+  const[name, setName] = useState("")
     
-    useEffect(() => {
+  useEffect(() => {
+      console.log('mise à jour du titre via useEffect') 
+      document.title = `vous avez cliqué ${count} fois`}, [] )  
+      setTimeout(()=>{
+              document.title = `Vous avez cliqué ${count} fois`;
+    },
+  1000)
 
-        console.log('mise à jour du titre via useEffect') 
-
-        document.title = `vous avez cliqué ${count} fois`}, [] )  
-
-        setTimeout(()=>{
-                document.title = `Vous avez cliqué ${count} fois`;
-        },1000)
-
-    return (
+  return (
     <div>
       <h1>{count}</h1>
       <input type='text' value={name} onChange={(e => setName(e.target.value))}></input>
       <button onClick={()=> setCount(count+1)}>cliques</button>
     </div>
   )
+  
 }
 
 export default FunctionCount
-
